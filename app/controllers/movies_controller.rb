75b9eb2662dfc8256e87308c15494018894eff30
Dorothy Rifai <drifai28@gmail.com>
2012-10-21 10:11:54 -0700

    Changes to accompany UI

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,13 @@
 class MoviesController < ApplicationController
 
+   attr_reader :all_ratings
+   def initialize
+     super()
+     logger.debug "Ratings #{Movie.all_ratings}"
+     @all_ratings = Movie.all_ratings
+     logger.debug "From controller  #{@all_ratings}"
+   end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,7 +15,14 @@ class MoviesController < ApplicationController
   end
 
   def index
+    Movie.filtered_sort('title', 'G')
     @movies = Movie.all
+    #@movies = Movie.order(params[:sort])
+#@movies = Movie.order(sort_column)
+    #ogger.debug "#{params}"
+#movies = Movie.filtered_sort(params[:sort], ratings)
+    #movies = Movie.filtered_sort('title', 'G')
+    #logger.debug('Movie path: ' + movies_path)
   end
 
   def new
@@ -37,5 +52,24 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+
+  def sort_column 
+     %w[title, release_date].include?(params[:sort]) ? params[:sort] : "title"
+  end
+
+  def ratings
+    ratings = params[:ratings]
+    logger.debug "#{ratings}"
+
+  end
+
+#def filter()
+#     collection = Movie.order(sort_column)
+#     collection.each do |movie|
+#        logger.debug "#{movie.title} #{movie.rating}"
+#     end
+#Movie.where('rating = "G"')
+#  end
+
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,46 @@
 class Movie < ActiveRecord::Base
+
+  def self.all_ratings
+   return ['G', 'PG', 'PG-13', 'R']
+  end
+
+  def self.filtered_sort(sort_param, filter_param)
+   #return(self.order(sort_param))
+   if(sort_param) === nil
+     sort_param = 'title'
+   end
+   #%w[title, release_date].include?(sort_param) ? sort_param : "title"
+   logger.debug 'Entering filtered sort'
+   logger.debug  "Filter: #{filter_param} #{filter_param.class}" 
+#for debugging
+#movies = Movie.order('title')
+#logger.debug "movies #{movies}"
+   #Movies = Movie.order(sort_param)
+   #if(filter_param === nil)
+   #  return movies
+   #end
+   #h = eval(filter_param)
+   #if(h === nil) 
+   #  logger.debug "Hash null... returning"
+   #  return movies
+   #end
+   #Array = Array.new
+   #h.each do |k,v|
+   #   logger.debug "#{k}"
+   #   array << k
+   #end
+  
+   #filtered_list = Array.new
+   #movies.each do |m|
+   # if(array.include?(m.rating.upcase))
+#if(m.rating.upcase === 'PG')
+   #     filtered_list << m
+   #  end
+   #end
+   #logger.debug 'Leaving filtered sort'
+   #logger.debug "#{filtered_list}"
+   #return(filtered_list)
+  end
+
+
 end
